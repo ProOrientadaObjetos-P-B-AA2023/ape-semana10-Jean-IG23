2f25799ae7e8a98c10f8a4353af6f8971e9e5c94
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-06-18 16:08:52 -0500

    Update Prestamo.java

--- ManejoHerencia/src/paquete2/Prestamo.java
+++ ManejoHerencia/src/paquete2/Prestamo.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete2;
 
-/**
- *
- * @author reroes
- */
 public class Prestamo {
-    
+    public Persona beneficiario;
+    public int prestamoMeses;
+    public String ciudadPrestamo;
+
+    public Prestamo(Persona beneficiario, int prestamoMeses, String ciudadPrestamo) {
+        this.beneficiario = beneficiario;
+        this.prestamoMeses = prestamoMeses;
+        this.ciudadPrestamo = ciudadPrestamo;
+    }
+
+    public void setCiudadPrestamo() {
+        this.ciudadPrestamo = ciudadPrestamo.toLowerCase();
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "beneficiario=" + beneficiario +
+                ", prestamoMeses=" + prestamoMeses +
+                ", ciudadPrestamo='" + ciudadPrestamo + '\'' +
+                '}';
+    }
 }
